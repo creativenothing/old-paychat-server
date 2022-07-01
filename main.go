702a6280a050b2bdb612af86f9fdb301c65a8216
creativenothing <creative@nothing>
2022-07-01 15:48:59 -0400

    fix whitespace

--- main.go
+++ main.go
@@ -5,15 +5,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
-    "fmt"
 	"net/http"
-    "encoding/json"
-    "io/ioutil"
 
-    "github.com/gorilla/sessions"
-
+	"github.com/gorilla/sessions"
 )
 
 var hubs map[string]*Hub = make(map[string]*Hub)
@@ -22,31 +21,32 @@ var addr = flag.String("addr", ":8080", "http service address")
 
 var clientNo = 0
 
-func corsHandle(w http.ResponseWriter, r *http.Request){
-    w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    w.Header().Set("Access-Control-Allow-Credentials", "true")
-    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
-    //w.Header().Set("Access-Control-Allow-Headers", "true")
+func corsHandle(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
+	//w.Header().Set("Access-Control-Allow-Headers", "true")
 }
+
 // Check if user is authenticated
-func validateSession(w http.ResponseWriter, r *http.Request) bool{
+func validateSession(w http.ResponseWriter, r *http.Request) bool {
 
-    session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, "cookie-name")
 
-    if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-        fmt.Printf("Validation failed %v\n", session.Values )
-        http.Error(w, "Forbidden", http.StatusForbidden)
-        return false
-    }
-    fmt.Printf("Validation succeeded %v\n", session.Values )
-    return true
+	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+		fmt.Printf("Validation failed %v\n", session.Values)
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return false
+	}
+	fmt.Printf("Validation succeeded %v\n", session.Values)
+	return true
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	corsHandle(w,r)
+	corsHandle(w, r)
 
-    log.Println(r.URL)
+	log.Println(r.URL)
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
 		return
@@ -59,141 +59,138 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 }
 
 var (
-    // key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-    key = []byte("super-secret-key")
-    store = sessions.NewCookieStore(key)
+	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
+	key   = []byte("super-secret-key")
+	store = sessions.NewCookieStore(key)
 )
 
 func secret(w http.ResponseWriter, r *http.Request) {
-	corsHandle(w,r)
+	corsHandle(w, r)
 
-    if valid := validateSession(w,r); !valid {
-        return
-    }
+	if valid := validateSession(w, r); !valid {
+		return
+	}
 
-    fmt.Println("Secret called")
+	fmt.Println("Secret called")
 
-
-    // Print secret message
-    fmt.Fprintln(w, "The cake is a lie!")
+	// Print secret message
+	fmt.Fprintln(w, "The cake is a lie!")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	corsHandle(w,r)
+	corsHandle(w, r)
 
-    fmt.Printf("Login called %s\n",r.Method)
-    if(r.Method == "OPTIONS"){
-        return
-    }
-    if(r.Method != "POST"){
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	fmt.Printf("Login called %s\n", r.Method)
+	if r.Method == "OPTIONS" {
 		return
-    }
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-    body, err:= ioutil.ReadAll(r.Body)
-    if err != nil{
-        fmt.Printf("read error: %s\n", err)
-        return
-    }
-    postJSON := map[string]interface{}{}
-    if err := json.Unmarshal(body, &postJSON) ; err != nil {
-        fmt.Printf("read error: %s\n", err)
-        return
-    }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("read error: %s\n", err)
+		return
+	}
+	postJSON := map[string]interface{}{}
+	if err := json.Unmarshal(body, &postJSON); err != nil {
+		fmt.Printf("read error: %s\n", err)
+		return
+	}
 
-	fmt.Printf("%s\n",body)
+	fmt.Printf("%s\n", body)
 
-    username := postJSON["username"].(string)
-    fmt.Println(username)
+	username := postJSON["username"].(string)
+	fmt.Println(username)
 
-    session, _ := store.Get(r, "cookie-name")
-    //password := r.FormValue("password")
+	session, _ := store.Get(r, "cookie-name")
+	//password := r.FormValue("password")
 
-    // Authentication goes here
-    // ...
+	// Authentication goes here
+	// ...
 
-    // Set user as authenticated
+	// Set user as authenticated
 
-    clientNo++
-    user:= User{
-        username: username,
-        userid: clientNo,
-    }
+	clientNo++
+	user := User{
+		username: username,
+		userid:   clientNo,
+	}
 
-    user = user
+	user = user
 
-    userid:= clientNo
-    //username:= fmt.Sprintf("User %d",clientNo)
+	userid := clientNo
+	//username:= fmt.Sprintf("User %d",clientNo)
 
+	session.Values = map[interface{}]interface{}{}
+	writeUser(session.Values, user)
+	//session.Values["username"] = username
 
-    session.Values = map[interface{}]interface{}{}
-    writeUser(session.Values, user)
-    //session.Values["username"] = username
+	session.Values["authenticated"] = true
+	fmt.Printf("%v\n", session.Values)
+	session.Save(r, w)
 
-    session.Values["authenticated"] = true
-    fmt.Printf("%v\n", session.Values)
-    session.Save(r, w)
-
-    msgJSON, _ := json.Marshal(
-        map[string]interface{}{
-            "id": userid,
-            "username": username,
-        })
-    fmt.Fprint(w, string(msgJSON))
+	msgJSON, _ := json.Marshal(
+		map[string]interface{}{
+			"id":       userid,
+			"username": username,
+		})
+	fmt.Fprint(w, string(msgJSON))
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-	corsHandle(w,r)
+	corsHandle(w, r)
 
-    session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, "cookie-name")
 
-    // Revoke users authentication
-    session.Values["authenticated"] = false
-    session.Save(r, w)
+	// Revoke users authentication
+	session.Values["authenticated"] = false
+	session.Save(r, w)
 }
 
 func auth(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Auth called")
-	corsHandle(w,r)
+	fmt.Println("Auth called")
+	corsHandle(w, r)
 
-    if valid := validateSession(w,r); !valid {
-        return
-    }
-    session, _ := store.Get(r, "cookie-name")
+	if valid := validateSession(w, r); !valid {
+		return
+	}
+	session, _ := store.Get(r, "cookie-name")
 
-    user := readUser(session.Values)
+	user := readUser(session.Values)
 
-    msgJSON, _ := json.Marshal(
-        map[string]interface{}{
-            "id": user.userid,
-            "username": user.username,
-        })
+	msgJSON, _ := json.Marshal(
+		map[string]interface{}{
+			"id":       user.userid,
+			"username": user.username,
+		})
 
-    fmt.Println(string(msgJSON))
-    fmt.Fprint(w, string(msgJSON))
+	fmt.Println(string(msgJSON))
+	fmt.Fprint(w, string(msgJSON))
 }
 
-func websocketEndpoint(w http.ResponseWriter, r *http.Request){
-    corsHandle(w,r)
+func websocketEndpoint(w http.ResponseWriter, r *http.Request) {
+	corsHandle(w, r)
 
-    if valid := validateSession(w,r); !valid {
-        return
-    }
+	if valid := validateSession(w, r); !valid {
+		return
+	}
 
-    serveWs(w, r)
+	serveWs(w, r)
 }
-
 
 func main() {
 	flag.Parse()
-    hub := newHub()
+	hub := newHub()
 	go hub.run()
-    hubs["main"] = hub
+	hubs["main"] = hub
 
-    http.HandleFunc("/secret", secret)
-    http.HandleFunc("/login", login)
-    http.HandleFunc("/logout", logout)
-    http.HandleFunc("/auth", auth)
+	http.HandleFunc("/secret", secret)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/auth", auth)
 
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", websocketEndpoint)
